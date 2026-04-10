ddbccd8a6f16697a6da1f378bb4b487a7151092e
大可 <79038729+wangqi2002@users.noreply.github.com>
2026-04-10 10:08:35 +0800

    Add files via upload

---
+++ Fun-ASR/demo2.py
@@ -0,0 +1,22 @@
+import torch
+from model import FunASRNano
+
+
+def main():
+    model_dir = "FunAudioLLM/Fun-ASR-Nano-2512"
+    device = (
+        "cuda:0"
+        if torch.cuda.is_available()
+        else "mps" if torch.backends.mps.is_available() else "cpu"
+    )
+    m, kwargs = FunASRNano.from_pretrained(model=model_dir, device=device)
+    m.eval()
+
+    wav_path = f"{kwargs['model_path']}/example/zh.mp3"
+    res = m.inference(data_in=[wav_path], **kwargs)
+    text = res[0][0]["text"]
+    print(text)
+
+
+if __name__ == "__main__":
+    main()
